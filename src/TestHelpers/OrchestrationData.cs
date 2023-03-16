a85766bb4bee283094260d20f008b1ace08696ef
Michael Young <mick.young1981@googlemail.com>
2023-03-16 12:47:15 +0000

    Initial structure

---
+++ src/TestHelpers/OrchestrationData.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace SFA.DAS.Funding.ApprenticeshipPayments.TestHelpers;
+
+public class OrchestrationData : IOrchestrationData
+{
+    public DurableOrchestrationStatus Status { get; set; }
+    public object Entity { get; set; }
+}
