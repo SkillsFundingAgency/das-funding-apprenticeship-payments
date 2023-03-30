86cdb02528abc85193be6529d995e9b2073864c2
Michael Young <mick.young1981@googlemail.com>
2023-03-30 14:35:44 +0100

    TEST WIP

---
+++ src/AcceptanceTests/Features/Features/CalculatePaymentsFromEarnings.feature
@@ -0,0 +1,8 @@
+﻿Feature: Calculate payments for earnings
+
+Scenario: Simple Payments Generation
+	Given earnings have been generated
+	And all of the earnings are due in the future
+	And no payments have previously been generated
+	When payments are calculated
+	Then Payments are generated with the correct learning amounts
---
+++ src/AcceptanceTests/StepDefinitions/PaymentsGeneratedEventHandlingStepDefinitions.cs
@@ -0,0 +1,46 @@
+using NServiceBus;
+using SFA.DAS.Funding.ApprenticeshipPayments.AcceptanceTests.Handlers;
+using SFA.DAS.Funding.ApprenticeshipPayments.AcceptanceTests.Helpers;
+using SFA.DAS.Funding.ApprenticeshipPayments.TestHelpers;
+using SFA.DAS.Funding.ApprenticeshipPayments.Types;
+
+namespace SFA.DAS.Funding.ApprenticeshipPayments.AcceptanceTests.StepDefinitions;
+
+[Binding]
+public class PaymentsGeneratedEventHandlingStepDefinitions
+{
+    private readonly ScenarioContext _scenarioContext;
+    private static IEndpointInstance _endpointInstance;
+
+    public PaymentsGeneratedEventHandlingStepDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [BeforeTestRun]
+    public static async Task StartEndpoint()
+    {
+        _endpointInstance = await EndpointHelper
+            .StartEndpoint("Test.Funding.ApprenticeshipPayments", false, new[] { typeof(PaymentsGeneratedEvent) });
+    }
+
+    [AfterTestRun]
+    public static async Task StopEndpoint()
+    {
+        await _endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+
+    [Then(@"Payments are generated with the correct learning amounts")]
+    public async Task AssertEarningsGeneratedEvent()
+    {
+        await WaitHelper.WaitForIt(() => PaymentsGeneratedEventHandler.ReceivedEvents.Any(EventMatchesExpectation), "Failed to find published PaymentsGenerated event");
+    }
+
+    private bool EventMatchesExpectation(PaymentsGeneratedEvent paymentsGeneratedEvent)
+    {
+        return paymentsGeneratedEvent.ApprenticeshipKey == (Guid) _scenarioContext["apprenticeshipKey"] &&
+               paymentsGeneratedEvent.Payments.Count() == (int) _scenarioContext["numberOfPayments"] &&
+               paymentsGeneratedEvent.Payments.All(x => x.Amount == (decimal) _scenarioContext["paymentAmount"]);
+    }
+}
