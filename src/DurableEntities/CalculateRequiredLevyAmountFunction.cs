678d229853e1faf1311f56f8f86841a3e565769d
Rus Scelkunovs <ruslanss@gmail.com>
2023-06-15 12:55:39 +0100

    fix func start-up

--- src/DurableEntities/CalculateRequiredLevyAmountFunction.cs
+++ src/DurableEntities/CalculateRequiredLevyAmountFunction.cs
@@ -9,16 +9,23 @@ namespace SFA.DAS.Funding.ApprenticeshipPayments.DurableEntities
 {
     public class CalculateRequiredLevyAmountFunction
     {
+        private readonly ICalculateRequiredLevyAmountCommandHandler _commandHandler;
+
+        public CalculateRequiredLevyAmountFunction(ICalculateRequiredLevyAmountCommandHandler commandHandler)
+        {
+            _commandHandler = commandHandler;
+        }
+
         [FunctionName(nameof(CalculateRequiredLevyAmountFunction))]
         public async Task Run(
             [NServiceBusTrigger(Endpoint = QueueNames.FinalisedOnProgammeLearningPayment)] FinalisedOnProgammeLearningPaymentEvent @event,
-            ICalculateRequiredLevyAmountCommandHandler commandHandler,
+            
             ILogger log)
         {
             log.LogInformation(
                 $"Triggered {nameof(CalculateRequiredLevyAmountFunction)} function for Apprenticeship: {@event.ApprenticeshipKey}");
 
-            await commandHandler.Process(new CalculateRequiredLevyAmountCommand(@event));
+            await _commandHandler.Process(new CalculateRequiredLevyAmountCommand(@event));
         }
     }
 }
