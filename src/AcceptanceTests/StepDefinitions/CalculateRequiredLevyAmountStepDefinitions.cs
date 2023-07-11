955858449c25c38ee8933b5352827776cf55bf53
Tom Gough <tom.gough@talentconsulting.uk>
2023-07-11 13:47:26 +0100

    acceptance test assertion for JobId default on CalculatedRequiredLevyAmount

--- src/AcceptanceTests/StepDefinitions/CalculateRequiredLevyAmountStepDefinitions.cs
+++ src/AcceptanceTests/StepDefinitions/CalculateRequiredLevyAmountStepDefinitions.cs
@@ -91,6 +91,7 @@ public class CalculateRequiredLevyAmountStepDefinitions
         outboundEvent.TransferSenderAccountId.Should().Be(inboundEvent.EmployerDetails.FundingAccountId);
         outboundEvent.Ukprn.Should().Be(inboundEvent.ApprenticeshipEarning.ProviderIdentifier);
         outboundEvent.TransactionType.Should().Be(TransactionType.Learning);
+        outboundEvent.JobId.Should().Be(-1);
 
         return true;
     }
