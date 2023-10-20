2107e90ad9c654418ae238cea0e14effe0453e1e
Tom Gough <tom.gough@talentconsulting.uk>
2023-10-20 16:22:39 +0100

    FLP-350 Domain unit tests

---
+++ src/Domain.UnitTests/Apprenticeship/WhenClearEarnings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Funding.ApprenticeshipPayments.Domain.Apprenticeship;
+
+namespace SFA.DAS.Funding.ApprenticeshipPayments.Domain.UnitTests.Apprenticeship;
+
+[TestFixture]
+public class WhenClearEarnings
+{
+    private Fixture _fixture;
+    private Domain.Apprenticeship.Apprenticeship _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fixture = new Fixture();
+        _sut = new Domain.Apprenticeship.Apprenticeship(Guid.NewGuid(), _fixture.CreateMany<Earning>().ToList(), new List<Payment>());
+    }
+
+    [Test]
+    public void AllExistingEarningsShouldBeCleared()
+    {
+        _sut.ClearEarnings();
+        _sut.Earnings.Should().BeEmpty();
+    }
+}
