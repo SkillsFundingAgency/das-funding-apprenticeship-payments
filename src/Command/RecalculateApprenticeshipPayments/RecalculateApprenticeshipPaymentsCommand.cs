dd27d237bc80e5855b4b74459544eea10da2affd
Tom Gough <tom.gough@talentconsulting.uk>
2023-10-20 16:23:12 +0100

    FLP-350 New command handler

--- src/Command/RecalculateApprenticeshipPayments/RecalculateApprenticeshipPaymentsCommand.cs
+++ src/Command/RecalculateApprenticeshipPayments/RecalculateApprenticeshipPaymentsCommand.cs
@@ -6,37 +6,13 @@ namespace SFA.DAS.Funding.ApprenticeshipPayments.Command.RecalculateApprenticesh
 {
     public class RecalculateApprenticeshipPaymentsCommand
     {
-        public RecalculateApprenticeshipPaymentsCommand(ApprenticeshipEntityModel apprenticeshipEntity, List<EarningsRecalculatedDeliveryPeriod> newEarnings)
+        public RecalculateApprenticeshipPaymentsCommand(ApprenticeshipEntityModel apprenticeshipEntity, List<Earning> newEarnings)
         {
             ApprenticeshipEntity = apprenticeshipEntity;
             NewEarnings = newEarnings;
         }
 
         public ApprenticeshipEntityModel ApprenticeshipEntity { get; }
-        public List<EarningsRecalculatedDeliveryPeriod> NewEarnings { get; set; }
-    }
-
-    //todo where should this live and should it be scoped to this command or a reusable concept of a delivery period that is not either tied to an output event or party of the entity model?
-    public class EarningsRecalculatedDeliveryPeriod
-    {
-        public byte Period { get; set; }
-        public byte CalendarMonth { get; set; }
-        public short CalenderYear { get; set; }
-        public short AcademicYear { get; set; }
-        public Decimal LearningAmount { get; set; }
-        public string FundingLineType { get; set; }
-    }
-
-    public interface IRecalculateApprenticeshipPaymentsCommandHandler
-    {
-        Task<Apprenticeship> Calculate(RecalculateApprenticeshipPaymentsCommand command);
-    }
-
-    public class RecalculateApprenticeshipPaymentsCommandHandler : IRecalculateApprenticeshipPaymentsCommandHandler
-    {
-        public Task<Apprenticeship> Calculate(RecalculateApprenticeshipPaymentsCommand command)
-        {
-            throw new NotImplementedException();
-        }
+        public List<Earning> NewEarnings { get; set; }
     }
 }
