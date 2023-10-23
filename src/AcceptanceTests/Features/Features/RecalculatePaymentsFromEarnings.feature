fa8281d9fb7fdaafe57e0117d1eda353abbf0182
Tom Gough <tom.gough@talentconsulting.uk>
2023-10-23 18:57:36 +0100

    Acceptance tests

---
+++ src/AcceptanceTests/Features/Features/RecalculatePaymentsFromEarnings.feature
@@ -0,0 +1,12 @@
+﻿@EarningsGeneratedEndpoint
+@PaymentsGeneratedEndpoint
+@ReleasePaymentsEndpoint
+@EarningsRecalculatedEndpoint
+
+Feature: Recalculate payments for earnings
+
+Scenario: Payments Recalculation
+	Given some previous earnings have been paid
+	And recalculated earnings are generated
+	When payments are recalculated
+	Then new payments are generated with the correct learning amounts
--- src/AcceptanceTests/Helpers/PeriodHelper.cs
+++ src/AcceptanceTests/Helpers/PeriodHelper.cs
@@ -1,4 +1,5 @@
 ﻿using SFA.DAS.Funding.ApprenticeshipEarnings.Types;
+using SFA.DAS.Funding.ApprenticeshipPayments.DurableEntities;
 
 namespace SFA.DAS.Funding.ApprenticeshipPayments.AcceptanceTests.Helpers;
 
@@ -7,6 +8,15 @@ public static class PeriodHelper
     public static void SetDeliveryPeriodsAccordingToCalendarMonths(this EarningsGeneratedEvent earningsGeneratedEvent)
     {
         foreach (var deliveryPeriod in earningsGeneratedEvent.DeliveryPeriods)
+        {
+            deliveryPeriod.AcademicYear = deliveryPeriod.CalenderYear.ToAcademicYear(deliveryPeriod.CalendarMonth);
+            deliveryPeriod.Period = deliveryPeriod.CalendarMonth.ToDeliveryPeriod();
+        }
+    }
+
+    public static void SetDeliveryPeriodsAccordingToCalendarMonths(this ApprenticeshipEarningsRecalculatedEvent earningsRecalculatedEvent)
+    {
+        foreach (var deliveryPeriod in earningsRecalculatedEvent.DeliveryPeriods)
         {
             deliveryPeriod.AcademicYear = deliveryPeriod.CalenderYear.ToAcademicYear(deliveryPeriod.CalendarMonth);
             deliveryPeriod.Period = deliveryPeriod.CalendarMonth.ToDeliveryPeriod();
