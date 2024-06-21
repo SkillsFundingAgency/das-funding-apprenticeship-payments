acb148e7ecdceca5fc3f4f05bb2f12785ab8812a
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-06-21 13:58:13 +0100

    Update PaymentsFunctions.cs

--- src/DurableEntities/PaymentsFunctions.cs
+++ src/DurableEntities/PaymentsFunctions.cs
@@ -10,39 +10,43 @@ public class PaymentsFunctions
         [DurableClient] IDurableEntityClient client,
         ILogger log)
     {
-        using CancellationTokenSource source = new CancellationTokenSource();
-        var token = source.Token;
-        var allApprenticeshipEntitiesQuery = new EntityQuery { EntityName = nameof(ApprenticeshipEntity) }; //default page size is 100, we may wish to tweak this in future to improve performance
-        var pageCounter = 0;
-
-        do
+        using (var releasePaymentsMonitor = log.LogPerformance("ReleasePaymentsEventServiceBusTrigger"))
         {
-            using (log.BeginScope("CorrelationId: {CorrelationId}", Guid.NewGuid()))
+            using CancellationTokenSource source = new CancellationTokenSource();
+            var token = source.Token;
+            var allApprenticeshipEntitiesQuery = new EntityQuery { EntityName = nameof(ApprenticeshipEntity) }; //default page size is 100, we may wish to tweak this in future to improve performance
+            var pageCounter = 0;
+
+            do
             {
-                pageCounter++;
-
-                var monitorListEntitiesAsync = log.LogPerformance("ListEntitiesAsync");
-
-                await client.CleanEntityStorageAsync(true, true, token);
-                var result = await client.ListEntitiesAsync(allApprenticeshipEntitiesQuery, token);
-                var releasePaymentsTasks = result.Entities.Select(x => client.SignalEntityAsync(x.EntityId, nameof(ApprenticeshipEntity.ReleasePaymentsForCollectionPeriod), releasePaymentsCommand));
-
-                monitorListEntitiesAsync.Dispose();
-
-                allApprenticeshipEntitiesQuery.ContinuationToken = result.ContinuationToken;
-
-                log.LogInformation($"Releasing payments for collection period {releasePaymentsCommand.CollectionPeriod} & year {releasePaymentsCommand.CollectionYear} for page {pageCounter} of entities. (Count: {result.Entities.Count()})");
-
-                using (var monitor = log.LogPerformance("Release Tasks"))
+                using (log.BeginScope("Loop CorrelationId: {loop_correlationId}", Guid.NewGuid()))
                 {
-                    await Task.WhenAll(releasePaymentsTasks);
+                    pageCounter++;
+
+                    var monitorListEntitiesAsync = log.LogPerformance("ListEntitiesAsync");
+
+                    await client.CleanEntityStorageAsync(true, true, token);
+                    var result = await client.ListEntitiesAsync(allApprenticeshipEntitiesQuery, token);
+                    var releasePaymentsTasks = result.Entities.Select(x => client.SignalEntityAsync(x.EntityId, nameof(ApprenticeshipEntity.ReleasePaymentsForCollectionPeriod), releasePaymentsCommand));
+
+                    monitorListEntitiesAsync.Dispose();
+
+                    allApprenticeshipEntitiesQuery.ContinuationToken = result.ContinuationToken;
+
+                    log.LogInformation($"Releasing payments for collection period {releasePaymentsCommand.CollectionPeriod} & year {releasePaymentsCommand.CollectionYear} for page {pageCounter} of entities. (Count: {result.Entities.Count()})");
+
+                    using (var monitor = log.LogPerformance("Release Tasks"))
+                    {
+                        await Task.WhenAll(releasePaymentsTasks);
+                    }
                 }
-            }
 
-                
 
-        } while (allApprenticeshipEntitiesQuery.ContinuationToken != null);
 
-        log.LogInformation($"Releasing payments for collection period {releasePaymentsCommand.CollectionPeriod} & year {releasePaymentsCommand.CollectionYear} complete.");
+            } while (allApprenticeshipEntitiesQuery.ContinuationToken != null);
+
+            log.LogInformation($"Releasing payments for collection period {releasePaymentsCommand.CollectionPeriod} & year {releasePaymentsCommand.CollectionYear} complete.");
+        }
+
     }
 }
