f306abdfb8485397b33aec277aefc974d05f1960
Michael Young <mick.young1981@googlemail.com>
2024-10-22 12:49:11 +0100

    Missed files

---
+++ src/SFA.DAS.ApprenticeshipPayments.Query/GetLearnersInILR/GetLearnersInILRQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Funding.ApprenticeshipPayments.Query.GetLearnersInILR
+{
+    public class GetLearnersInILRQuery
+    {
+        public long Ukprn { get; }
+
+        public GetLearnersInILRQuery(long ukprn)
+        {
+            Ukprn = ukprn;
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeshipPayments.Query/GetLearnersInILR/IGetLearnersInILRQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Funding.ApprenticeshipPayments.Query.GetLearnersInILR
+{
+    public interface IGetLearnersInILRQueryHandler
+    {
+        Task<GetLearnersInILRQueryResponse> Get(GetLearnersInILRQuery command);
+    }
+}
