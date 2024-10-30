9985d9f8fb79c2638582f4f59ffc1efe5c15cbcc
tom-gough <tom.gough.tgsd@outlook.com>
2024-10-30 08:17:55 +0000

    FLP-977 Updated payments recalculation to prevent generation of zero payments where the correct amount was already paid from a previous earnings profile

--- src/Domain.UnitTests/Apprenticeship/WhenRecalculatePayments.cs
+++ src/Domain.UnitTests/Apprenticeship/WhenRecalculatePayments.cs
@@ -29,20 +29,23 @@ public class WhenRecalculatePayments
         _newEarningsProfileId = Guid.NewGuid();
 
         //Expectation:
-        //Delivery Period 1 - diff payment calculated
-        //Delivery Period 2 - unpaid payment removed and new one calculated for new learning amount
-        //Delivery Period 3 - no existing payments, new one calculated for new learning amount
+        //Delivery Period 1 - existing payment matches earning, no payment to be generated (e.g. if there was a price change after this period)
+        //Delivery Period 2 - diff payment calculated
+        //Delivery Period 3 - unpaid payment removed and new one calculated for new learning amount
+        //Delivery Period 4 - no existing payments, new one calculated for new learning amount
         _newEarnings = new List<Earning>
         {
-            new Earning(2223, 1, _newMonthlyLearningAmount, 2022, 10, _fixture.Create<string>(), _newEarningsProfileId),
-            new Earning(2223, 2, _newMonthlyLearningAmount, 2022, 10, _fixture.Create<string>(), _newEarningsProfileId),
-            new Earning(2223, 3, _newMonthlyLearningAmount, 2022, 10, _fixture.Create<string>(), _newEarningsProfileId)
+            new Earning(2223, 1, _currentMonthlyLearningAmount, 2022, 11, _fixture.Create<string>(), _newEarningsProfileId),
+            new Earning(2223, 2, _newMonthlyLearningAmount, 2022, 11, _fixture.Create<string>(), _newEarningsProfileId),
+            new Earning(2223, 3, _newMonthlyLearningAmount, 2022, 11, _fixture.Create<string>(), _newEarningsProfileId),
+            new Earning(2223, 4, _newMonthlyLearningAmount, 2022, 11, _fixture.Create<string>(), _newEarningsProfileId)
         };
 
         _existingPayments = new List<Payment>()
         {
-            new Payment(2223, 1, _currentMonthlyLearningAmount, 2022, 8, "payment 1 paid", _previousEarningsProfileId){ SentForPayment = true },
-            new Payment(2223, 2, _currentMonthlyLearningAmount, 2022, 8, "payment 2 unpaid", _previousEarningsProfileId)
+            new Payment(2223, 1, _currentMonthlyLearningAmount, 2022, 9, "payment 1 paid", _previousEarningsProfileId){ SentForPayment = true },
+            new Payment(2223, 2, _currentMonthlyLearningAmount, 2022, 9, "payment 2 paid", _previousEarningsProfileId){ SentForPayment = true },
+            new Payment(2223, 3, _currentMonthlyLearningAmount, 2022, 9, "payment 3 unpaid", _previousEarningsProfileId)
         };
 
         _sut = new Domain.Apprenticeship.Apprenticeship(Guid.NewGuid(), new List<Earning>(), _existingPayments);
@@ -58,30 +61,37 @@ public class WhenRecalculatePayments
     [Test]
     public void ExistingUnpaidPaymentsShouldBeRemoved()
     {
-        _sut.Payments.Should().NotContain(p => p.FundingLineType == "payment 2 unpaid");
+        _sut.Payments.Should().NotContain(p => p.FundingLineType == "payment 3 unpaid");
     }
 
     [Test]
     public void ExistingPaidPaymentsShouldBeNotBeRemoved()
     {
         _sut.Payments.Should().Contain(p => p.FundingLineType == "payment 1 paid" && p.EarningsProfileId == _previousEarningsProfileId);
+        _sut.Payments.Should().Contain(p => p.FundingLineType == "payment 2 paid" && p.EarningsProfileId == _previousEarningsProfileId);
     }
 
     [Test]
     public void NewPaymentsToMakeUpExistingPaidPaymentsAmountsShouldBeAdded()
     {
-        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 1 && p.Amount == _newMonthlyLearningAmount - _currentMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
+        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 2 && p.Amount == _newMonthlyLearningAmount - _currentMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
     }
 
     [Test]
     public void NewPaymentsForPreviouslyRemovedUnpaidPaymentPeriodsShouldBeAdded()
     {
-        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 2 && p.Amount == _newMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
+        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 3 && p.Amount == _newMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
     }
 
     [Test]
     public void NewPaymentsForPreviouslyUncalculatedPaymentPeriodsShouldBeAdded()
     {
-        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 3 && p.Amount == _newMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
+        _sut.Payments.Should().Contain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 4 && p.Amount == _newMonthlyLearningAmount && p.EarningsProfileId == _newEarningsProfileId);
+    }
+
+    [Test]
+    public void PaymentsForPreviouslyPaidPaymentPeriodsWithNoChangeShouldNotBeAdded()
+    {
+        _sut.Payments.Should().NotContain(p => p.AcademicYear == 2223 && p.DeliveryPeriod == 1 && p.Amount == 0);
     }
 }
