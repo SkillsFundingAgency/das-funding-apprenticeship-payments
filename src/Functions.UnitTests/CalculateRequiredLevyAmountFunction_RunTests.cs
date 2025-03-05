0cd439bb5466d1ae7ac49c85bf05e61343ba5630
Chris Foster <chrisfoster186@googlemail.com>
2025-03-05 15:55:12 +0000

    WIP

--- src/Functions.UnitTests/CalculateRequiredLevyAmountFunction_RunTests.cs
+++ src/Functions.UnitTests/CalculateRequiredLevyAmountFunction_RunTests.cs
@@ -1,35 +1,35 @@
-﻿using System.Threading.Tasks;
-using AutoFixture;
-using Microsoft.Extensions.Logging;
-using Moq;
-using NUnit.Framework;
-using SFA.DAS.Funding.ApprenticeshipPayments.Command;
-using SFA.DAS.Funding.ApprenticeshipPayments.Command.CalculateRequiredLevyAmount;
-using SFA.DAS.Funding.ApprenticeshipPayments.Types;
+﻿//using System.Threading.Tasks;
+//using AutoFixture;
+//using Microsoft.Extensions.Logging;
+//using Moq;
+//using NUnit.Framework;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Command;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Command.CalculateRequiredLevyAmount;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Types;
 
-namespace SFA.DAS.Funding.ApprenticeshipPayments.Functions.UnitTests;
+//namespace SFA.DAS.Funding.ApprenticeshipPayments.Functions.UnitTests;
 
-public class CalculateRequiredLevyAmountFunction_RunTests
-{
-    private readonly CalculateRequiredLevyAmountFunction _sut;
-    private readonly Mock<ICommandHandler<CalculateRequiredLevyAmountCommand>> _commandHandlerMock = new();
-    private readonly Fixture _fixture = new();
+//public class CalculateRequiredLevyAmountFunction_RunTests
+//{
+//    private readonly CalculateRequiredLevyAmountFunction _sut;
+//    private readonly Mock<ICommandHandler<CalculateRequiredLevyAmountCommand>> _commandHandlerMock = new();
+//    private readonly Fixture _fixture = new();
 
-    public CalculateRequiredLevyAmountFunction_RunTests()
-    {
-        _sut = new CalculateRequiredLevyAmountFunction(_commandHandlerMock.Object, Mock.Of<ILogger<CalculateRequiredLevyAmountFunction>>());
-    }
+//    public CalculateRequiredLevyAmountFunction_RunTests()
+//    {
+//        _sut = new CalculateRequiredLevyAmountFunction(_commandHandlerMock.Object, Mock.Of<ILogger<CalculateRequiredLevyAmountFunction>>());
+//    }
 
-    [Test]
-    public async Task Run_PublishesCalculateRequiredLevyAmountCommand()
-    {
-        // Arrange
-        var @event = _fixture.Create<FinalisedOnProgammeLearningPaymentEvent>();
+//    [Test]
+//    public async Task Run_PublishesCalculateRequiredLevyAmountCommand()
+//    {
+//        // Arrange
+//        var @event = _fixture.Create<FinalisedOnProgammeLearningPaymentEvent>();
 
-        // Act
-        await _sut.Run(@event);
+//        // Act
+//        await _sut.Run(@event);
 
-        // Assert
-        _commandHandlerMock.Verify(_ => _.Handle(It.Is<CalculateRequiredLevyAmountCommand>(c => c.Data == @event)), Times.Once());
-    }
-}
+//        // Assert
+//        _commandHandlerMock.Verify(_ => _.Handle(It.Is<CalculateRequiredLevyAmountCommand>(c => c.Data == @event)), Times.Once());
+//    }
+//}
