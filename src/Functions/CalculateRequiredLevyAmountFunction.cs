7971ba8278bd62dfc8bff86563b33b0018c4a450
Chris Foster <chrisfoster186@googlemail.com>
2025-03-05 15:54:29 +0000

    WIP

--- src/Functions/CalculateRequiredLevyAmountFunction.cs
+++ src/Functions/CalculateRequiredLevyAmountFunction.cs
@@ -1,28 +1,28 @@
-using Microsoft.Azure.Functions.Worker;
-using SFA.DAS.Funding.ApprenticeshipPayments.Command;
-using SFA.DAS.Funding.ApprenticeshipPayments.Command.CalculateRequiredLevyAmount;
-using SFA.DAS.Funding.ApprenticeshipPayments.Types;
+//using Microsoft.Azure.Functions.Worker;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Command;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Command.CalculateRequiredLevyAmount;
+//using SFA.DAS.Funding.ApprenticeshipPayments.Types;
 
-namespace SFA.DAS.Funding.ApprenticeshipPayments.Functions;
+//namespace SFA.DAS.Funding.ApprenticeshipPayments.Functions;
 
-public class CalculateRequiredLevyAmountFunction
-{
-    private readonly ICommandHandler<CalculateRequiredLevyAmountCommand> _commandHandler;
-    private readonly ILogger<CalculateRequiredLevyAmountFunction> _logger;
+//public class CalculateRequiredLevyAmountFunction
+//{
+//    private readonly ICommandHandler<CalculateRequiredLevyAmountCommand> _commandHandler;
+//    private readonly ILogger<CalculateRequiredLevyAmountFunction> _logger;
 
-    public CalculateRequiredLevyAmountFunction(ICommandHandler<CalculateRequiredLevyAmountCommand> commandHandler, ILogger<CalculateRequiredLevyAmountFunction> logger)
-    {
-        _commandHandler = commandHandler;
-        _logger = logger;
-    }
+//    public CalculateRequiredLevyAmountFunction(ICommandHandler<CalculateRequiredLevyAmountCommand> commandHandler, ILogger<CalculateRequiredLevyAmountFunction> logger)
+//    {
+//        _commandHandler = commandHandler;
+//        _logger = logger;
+//    }
 
-    [Function(nameof(CalculateRequiredLevyAmountFunction))]
-    public async Task Run(
-        [ServiceBusTrigger(QueueNames.FinalisedOnProgammeLearningPayment)] FinalisedOnProgammeLearningPaymentEvent @event)
-    {
-        _logger.LogInformation("Triggered {0} function for ApprenticeshipKey: {1}", nameof(CalculateRequiredLevyAmountFunction), @event.ApprenticeshipKey);
-        _logger.LogInformation("ApprenticeshipKey: {0} Received FinalisedOnProgammeLearningPaymentEvent: {1}", @event.ApprenticeshipKey, @event.SerialiseForLogging());
+//    [Function(nameof(CalculateRequiredLevyAmountFunction))]
+//    public async Task Run(
+//        [ServiceBusTrigger(QueueNames.FinalisedOnProgammeLearningPayment)] FinalisedOnProgammeLearningPaymentEvent @event)
+//    {
+//        _logger.LogInformation("Triggered {0} function for ApprenticeshipKey: {1}", nameof(CalculateRequiredLevyAmountFunction), @event.ApprenticeshipKey);
+//        _logger.LogInformation("ApprenticeshipKey: {0} Received FinalisedOnProgammeLearningPaymentEvent: {1}", @event.ApprenticeshipKey, @event.SerialiseForLogging());
 
-        await _commandHandler.Handle(new CalculateRequiredLevyAmountCommand(@event));
-    }
-}
+//        await _commandHandler.Handle(new CalculateRequiredLevyAmountCommand(@event));
+//    }
+//}
---
+++ src/Functions/Handlers/FinalisedOnProgammeLearningPaymentEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+using SFA.DAS.Funding.ApprenticeshipPayments.Command;
+using SFA.DAS.Funding.ApprenticeshipPayments.Command.CalculateRequiredLevyAmount;
+using SFA.DAS.Funding.ApprenticeshipPayments.Types;
+
+namespace SFA.DAS.Funding.ApprenticeshipPayments.Functions.Handlers
+{
+    public class FinalisedOnProgammeLearningPaymentEventHandler(ICommandHandler<CalculateRequiredLevyAmountCommand> commandHandler, ILogger<FinalisedOnProgammeLearningPaymentEventHandler> logger) : IHandleMessages<FinalisedOnProgammeLearningPaymentEvent>
+    {
+        public async Task Handle(FinalisedOnProgammeLearningPaymentEvent message, IMessageHandlerContext context)
+        {
+            logger.LogInformation("Triggered {0} function for ApprenticeshipKey: {1}", nameof(FinalisedOnProgammeLearningPaymentEventHandler), message.ApprenticeshipKey);
+            logger.LogInformation("ApprenticeshipKey: {0} Received FinalisedOnProgammeLearningPaymentEvent: {1}", message.ApprenticeshipKey, message.SerialiseForLogging());
+
+            await commandHandler.Handle(new CalculateRequiredLevyAmountCommand(message));
+        }
+    }
+}
