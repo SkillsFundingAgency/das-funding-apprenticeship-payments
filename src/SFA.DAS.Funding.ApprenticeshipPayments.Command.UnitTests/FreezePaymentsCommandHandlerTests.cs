1274c94559c4d5be85113004e783ee40ec29df18
Chris Foster <chrisfoster186@googlemail.com>
2025-04-28 14:36:44 +0100

    Adds unit tests

---
+++ src/SFA.DAS.Funding.ApprenticeshipPayments.Command.UnitTests/FreezePaymentsCommandHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using Moq;
+using SFA.DAS.Funding.ApprenticeshipPayments.Command.FreezePayments;
+using SFA.DAS.Funding.ApprenticeshipPayments.DataAccess.Repositories;
+using SFA.DAS.Funding.ApprenticeshipPayments.Domain.Apprenticeship;
+
+namespace SFA.DAS.Funding.ApprenticeshipPayments.Command.UnitTests
+{
+    public class FreezePaymentsCommandHandlerTests
+    {
+        private Fixture _fixture = null!;
+        private Mock<IApprenticeship> _apprenticeshipMock = null!;
+        private FreezePaymentsCommand _command = null!;
+        private Mock<IApprenticeshipRepository> _repositoryMock = null!;
+        private FreezePaymentsCommandHandler _sut = null!;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _fixture = new Fixture();
+            _command = _fixture.Create<FreezePaymentsCommand>();
+
+            _apprenticeshipMock = new Mock<IApprenticeship>();
+            _repositoryMock = new Mock<IApprenticeshipRepository>();
+
+            // Setup repository mock behavior
+            _repositoryMock
+                .Setup(x => x.Get(_command.ApprenticeshipKey))
+                .ReturnsAsync(_apprenticeshipMock.Object);
+
+            _sut = new FreezePaymentsCommandHandler(_repositoryMock.Object);
+
+            // Execute handler
+            await _sut.Handle(_command);
+        }
+
+        [Test]
+        public void ThenPaymentsAreFrozen()
+        {
+            _apprenticeshipMock.Verify(x => x.FreezePayments(), Times.Once);
+        }
+
+        [Test]
+        public void ThenApprenticeshipIsUpdated()
+        {
+            _repositoryMock.Verify(x => x.Update(_apprenticeshipMock.Object), Times.Once);
+        }
+    }
+}
